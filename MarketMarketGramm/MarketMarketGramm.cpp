e2295b2fcf71981ab2aaf9ef6fcce859a7fadb2e
MkGodh <fenryrvla@yandex.ru>
2024-03-19 15:18:49 +0300

    Добавьте файлы проекта.

---
+++ MarketMarketGramm/MarketMarketGramm.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+
+
+int main(void) {
+	using namespace std;
+	setlocale(LC_ALL, "RUS");
+
+	float pricePerHundredGram;
+	float weight;
+	float discount;
+	cout << "Введите стоимость товара: ";
+	cin >> pricePerHundredGram;
+	cout << "Введите вес товара: ";
+	cin >> weight;
+	cout << "Введите размер скидки: ";
+	cin >> discount;
+	int totalSum = weight / 100 * pricePerHundredGram;
+
+	cout << "Цена за " << weight << " грамм продукта с учетом скидки составит: " << totalSum - (totalSum / 100 * discount);
+}
